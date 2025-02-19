cc85044ecb03f8a30b458b255f26216598d10e5d
LeonardoRomulo <leo.romulo15@gmail.com>
2025-02-19 15:42:55 -0300

    Aplicação que compara os salários e a inflação de 2010 a 2020

---
+++ index.js
@@ -0,0 +1,52 @@
+import dados from 'readline-sync';
+
+let inflacao = [
+    {ano: 2010, ipca: 5.91},
+    {ano: 2011, ipca: 6.50},
+    {ano: 2012, ipca: 5.84},
+    {ano: 2013, ipca: 5.91},
+    {ano: 2014, ipca: 6.41},
+    {ano: 2015, ipca: 10.67},
+    {ano: 2016, ipca: 6.29},
+    {ano: 2017, ipca: 2.95},
+    {ano: 2018, ipca: 3.75},
+    {ano: 2019, ipca: 4.31},
+    {ano: 2020, ipca: 4.52}
+];
+
+let salarioMinimo = [
+    {ano: 2010, salario:510.00 },
+    {ano: 2011, salario: 545.00},
+    {ano: 2012, salario: 622.00},
+    {ano: 2013, salario: 678.00},
+    {ano: 2014, salario: 724.00},
+    {ano: 2015, salario: 788.00},
+    {ano: 2016, salario: 880.00},
+    {ano: 2017, salario: 937.00},
+    {ano: 2018, salario: 954.00},
+    {ano: 2019, salario: 998.00},
+    {ano: 2020, salario: 1045.00}
+];
+
+console.log("1 - Listar os salários minímos de 20010 à 2020");
+console.log("2 - Listar o índice IPCA de 2010 à 2020");
+console.log("3 - Comparação entre o percentual de aumento salarial do IPCA");
+
+let escolha = dados.question("Digite o número da sua escolha: ");
+
+
+if(Number(escolha) == 1){
+    for(let salarios of salarioMinimo){
+
+        let salario = salarios.salario;
+        let ano = salarios.ano;
+
+        console.log(`Ano: ${ano}`);
+        console.log(`Salário: ${salario},00`);
+        console.log("\n");
+    }
+}else if(Number(escolha) == 2){
+    for(){
+        
+    }
+}
